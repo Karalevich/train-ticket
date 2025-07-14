b20c9744f8bc3b6a93f41b648a7899060cfa668a
Karalevich <karalevichandrei@gmail.com>
2025-07-13 19:59:28 -0700

    work on template for SeatsPage

--- src/app/order/@tickets/tickets/[...slug]/page.tsx
+++ src/app/order/@tickets/tickets/[...slug]/page.tsx
@@ -5,6 +5,7 @@ import { fetchSeats, TicketFilters } from '@/lib/api';
 import PassengerSelection from '@/components/seats/PassengerSelection';
 import Route from '@/components/seats/Route';
 import TotalPrice from '@/components/seats/TotalPrice';
+import { Tabs, TabsContent, TabsList, TabsTrigger } from '@/components/ui/tabs';
 
 export default async function TicketPage({
                                            params,
@@ -38,6 +39,38 @@ export default async function TicketPage({
           <Route/>
 
           <PassengerSelection/>
+
+          <Tabs defaultValue="1" className="gap-0">
+            <div className="bg-orange-50 px-4 py-1 w-full">
+              <span>Carts:</span>
+              <TabsList className="bg-transparent">
+                {there.map((cart, index) => <TabsTrigger value={`${index}`} key={index}>{index + 1}</TabsTrigger>)}
+              </TabsList>
+            </div>
+
+            {there.map((cart, index) =>
+              <TabsContent value={`${index}`} key={index}>
+                <div className="flex">
+                  <div className="bg-orange-50 flex flex-col items-center justify-between py-10 px-14">
+                    <p className="text-5xl">{index + 1}</p>
+                    <p className="text-xl">cart</p>
+                  </div>
+                  <div>
+                    <div className="flex items-center justify-between p-4">
+                      <span className="text-lg font-semibold">Coach: {cart.coach.name}</span>
+                      <span className="text-sm text-gray-500">Price: ${cart.coach.price}</span>
+                    </div>
+                    <div className="p-4">
+                      <p className="text-sm text-gray-600">Available Seats: {cart.coach.avaliable_seats}</p>
+                      <p className="text-sm text-gray-600">Total Seats: {cart.coach.have_express}</p>
+                    </div>
+                  </div>
+
+                </div>
+              </TabsContent>
+            )}
+
+          </Tabs>
 
 
           {/*{selectedSeats.length > 0 && <TotalPrice price={}/>}*/}
--- src/lib/api.ts
+++ src/lib/api.ts
@@ -103,24 +103,26 @@ export interface TicketsResponse {
 }
 
 export interface SeatsResponse {
-  _id: string
-  name: string
-  class_type: string
-  have_first_class: boolean
-  have_second_class: boolean
-  have_third_class: boolean
-  have_fourth_class: boolean
-  have_wifi: boolean
-  have_air_conditioning: boolean
-  have_express: boolean
-  price?: number
-  top_price?: number
-  bottom_price?: number
-  side_price?: number
-  linens_price?: number
-  wifi_price?: number
-  avaliable_seats: number
-  is_linens_included: boolean
+  coach: {
+    _id: string
+    name: string
+    class_type: string
+    have_first_class: boolean
+    have_second_class: boolean
+    have_third_class: boolean
+    have_fourth_class: boolean
+    have_wifi: boolean
+    have_air_conditioning: boolean
+    have_express: boolean
+    price?: number
+    top_price?: number
+    bottom_price?: number
+    side_price?: number
+    linens_price?: number
+    wifi_price?: number
+    avaliable_seats: number
+    is_linens_included: boolean
+  }
   seats: Array<{
     index: number
     available: boolean
